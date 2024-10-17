f9842c9f23ff24d07b01985182a1ea9926734190
Alerrandro <alerrandrofrederik@gmail.com>
2024-10-16 23:53:00 -0300

    adicionando resposta exercicio 02

---
+++ exercicio_02.py
@@ -0,0 +1,6 @@
+#Criar um programa onde o usuário digite dois valores e apareça a soma
+
+num1 = int(input("digite um valor: "))
+num2 = int(input("digite outro valor: "))
+
+print(f"a soma dos valores e {num1 + num2}")
